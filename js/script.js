56e285f7bc6ea180b41109484174f02cccf0936d
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-07-27 17:27:32 -0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const keys = document.querySelectorAll('.keys');
+const keys = document.querySelectorAll('.key');
 const checkbox = document.querySelector('.checkbox__keys');
 const switcher = document.querySelector('.switcher');
 const keysSection = document.querySelector('.piano__keys');
@@ -12,22 +12,22 @@ const handleMouseDown = (key) => {
     playNote(key.getAttribute('data-note'));
 
     if (key.className.includes('black')) {
-        key.classList.add('black--pressed')
-        return
+        key.classList.add('black--pressed');
+        return;
     }
 
-    key.style.background = '#ddd'
-
+    key.style.background = '#ddd';
 }
 
 const handleMouseUp = (key) => {
+    
     if (key.className.includes('black')) {
         key.classList.remove('black--pressed');
         return;
     }
 
     key.style.background = 'white';
-
+    
 }
 
 keys.forEach((key) => {
@@ -38,7 +38,7 @@ keys.forEach((key) => {
 checkbox.addEventListener('change', ({ target }) => {
     if (target.checked) {
         switcher.classList.add('switcher--active');
-        keysSection.classList.remove('disabled--keys');
+        keysSection.classList.remove('disabled-keys');
         return;
     }
     switcher.classList.remove('switcher--active');
@@ -47,57 +47,57 @@ checkbox.addEventListener('change', ({ target }) => {
 
 const keyDownMapper = {
 
-    "Tab": () => handleMouseDown(keys[0]),
-    "a": () => handleMouseDown(keys[1]),
-    "q": () => handleMouseDown(keys[2]),
-    "2": () => handleMouseDown(keys[3]),
-    "w": () => handleMouseDown(keys[4]),
-    "e": () => handleMouseDown(keys[5]),
-    "4": () => handleMouseDown(keys[6]),
-    "r": () => handleMouseDown(keys[7]),
-    "5": () => handleMouseDown(keys[8]),
-    "t": () => handleMouseDown(keys[9]),
-    "6": () => handleMouseDown(keys[10]),
-    "y": () => handleMouseDown(keys[11]),
-    "u": () => handleMouseDown(keys[12]),
-    "8": () => handleMouseDown(keys[13]),
-    "i": () => handleMouseDown(keys[14]),
-    "9": () => handleMouseDown(keys[15]),
-    "o": () => handleMouseDown(keys[16]),
-    "p": () => handleMouseDown(keys[17]),
-    "-": () => handleMouseDown(keys[18]),
-    "[": () => handleMouseDown(keys[19]),
-    "=": () => handleMouseDown(keys[20]),
-    "]": () => handleMouseDown(keys[21]),
-    "Backspace": () => handleMouseDown(keys[22]),
-    "\\": () => handleMouseDown(keys[23]),
+    "a": () => handleMouseDown(keys[0]),
+    "w": () => handleMouseDown(keys[1]),
+    "s": () => handleMouseDown(keys[2]),
+    "e": () => handleMouseDown(keys[3]),
+    "d": () => handleMouseDown(keys[4]),
+    "f": () => handleMouseDown(keys[5]),
+    "t": () => handleMouseDown(keys[6]),
+    "g": () => handleMouseDown(keys[7]),
+    "y": () => handleMouseDown(keys[8]),
+    "h": () => handleMouseDown(keys[9]),
+    "u": () => handleMouseDown(keys[10]),
+    "j": () => handleMouseDown(keys[11]),
+    "k": () => handleMouseDown(keys[12]),
+    "o": () => handleMouseDown(keys[13]),
+    "l": () => handleMouseDown(keys[14]),
+    "p": () => handleMouseDown(keys[15]),
+    "ç": () => handleMouseDown(keys[16]),
+    "~": () => handleMouseDown(keys[17]),
+    "[": () => handleMouseDown(keys[18]),
+    "]": () => handleMouseDown(keys[19]),
+    ";": () => handleMouseDown(keys[20]),
+    ".": () => handleMouseDown(keys[21]),
+    ",": () => handleMouseDown(keys[22]),
+    "m": () => handleMouseDown(keys[23]),
 }
 
 const keyUpMapper = {
-    "Tab": () => handleMouseUp(keys[0]),
-    "1": () => handleMouseUp(keys[1]),
-    "q": () => handleMouseUp(keys[2]),
-    "2": () => handleMouseUp(keys[3]),
-    "w": () => handleMouseUp(keys[4]),
-    "e": () => handleMouseUp(keys[5]),
-    "4": () => handleMouseUp(keys[6]),
-    "r": () => handleMouseUp(keys[7]),
-    "5": () => handleMouseUp(keys[8]),
-    "t": () => handleMouseUp(keys[9]),
-    "6": () => handleMouseUp(keys[10]),
-    "y": () => handleMouseUp(keys[11]),
-    "u": () => handleMouseUp(keys[12]),
-    "8": () => handleMouseUp(keys[13]),
-    "i": () => handleMouseUp(keys[14]),
-    "9": () => handleMouseUp(keys[15]),
-    "o": () => handleMouseUp(keys[16]),
-    "p": () => handleMouseUp(keys[17]),
-    "-": () => handleMouseUp(keys[18]),
-    "[": () => handleMouseUp(keys[19]),
-    "=": () => handleMouseUp(keys[20]),
-    "]": () => handleMouseUp(keys[21]),
-    "Backspace": () => handleMouseUp(keys[22]),
-    "\\": () => handleMouseUp(keys[23]),
+    "a": () => handleMouseUp(keys[0]),
+    "w": () => handleMouseUp(keys[1]),
+    "s": () => handleMouseUp(keys[2]),
+    "e": () => handleMouseUp(keys[3]),
+    "d": () => handleMouseUp(keys[4]),
+    "f": () => handleMouseUp(keys[5]),
+    "t": () => handleMouseUp(keys[6]),
+    "g": () => handleMouseUp(keys[7]),
+    "y": () => handleMouseUp(keys[8]),
+    "h": () => handleMouseUp(keys[9]),
+    "u": () => handleMouseUp(keys[10]),
+    "j": () => handleMouseUp(keys[11]),
+    "k": () => handleMouseUp(keys[12]),
+    "o": () => handleMouseUp(keys[13]),
+    "l": () => handleMouseUp(keys[14]),
+    "p": () => handleMouseUp(keys[15]),
+    "ç": () => handleMouseUp(keys[16]),
+    "~": () => handleMouseUp(keys[17]),
+    "[": () => handleMouseUp(keys[18]),
+    "~": () => handleMouseUp(keys[19]),
+    ";": () => handleMouseUp(keys[20]),
+    ".": () => handleMouseUp(keys[21]),
+    ",": () => handleMouseUp(keys[22]),
+    "m": () => handleMouseUp(keys[23]),
 }
 
 document.addEventListener('keydown', (event) => {
